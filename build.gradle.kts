66aa987b9c71a10dc46d5c53d95f1dba5c1938c9
dcdu <dcdu@cn.ibm.com>
2026-09-02 21:27:49 +0800

    chore: gradle root project with version catalog

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "code-compliance-platform"
+include(
+    "app-server",
+    "module-common",
+    "module-auth",
+    "module-user",
+    "module-project",
+    "module-checklist",
+    "module-rule",
+    "module-scan",
+    "module-engine-adapter",
+    "module-result",
+    "module-report",
+    "module-remediation",
+    "module-notification",
+    "module-openapi",
+    "module-admin",
+)
